cf4fcd29cfc92b6f94894dfa6a5172aa42ea6eb4
terryjung0244 <terryjung0244@gmail.com>
2022-04-04 21:05:03 -0400

    object.map

--- src/GridComponent.jsx
+++ src/GridComponent.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { object } from './object'
 
 const GridComponent = () => {
   return (
     <div>
-      GridComponent
+      <div>
+        {object.map((object, index) => {
+          return (
+            <div key={object.fruitName}>
+              <div>
+                {object.fruitName}
+              </div>
+              <div>
+                {object.fruitColor}
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
